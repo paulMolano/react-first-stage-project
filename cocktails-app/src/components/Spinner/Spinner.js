b67fc201c82edffce914cc4db80ecf0c27cbe9d0
paulMolano <paulmolanoo@gmail.com>
2021-10-21 12:21:17 +0200

    Spinner folder

---
+++ cocktails-app/src/components/Spinner/Spinner.js
@@ -0,0 +1,14 @@
+import "./Spinner.css";
+
+export const Spinner = () => {
+  return (
+    <div class="sk-chase">
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+      <div class="sk-chase-dot"></div>
+    </div>
+  );
+};

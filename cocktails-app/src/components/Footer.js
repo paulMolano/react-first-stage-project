a56bc7824addc320dd11388c63b933f0a170b348
paulMolano <paulmolanoo@gmail.com>
2021-10-24 20:52:21 +0200

    footer build

---
+++ cocktails-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer
+      className="bg-dark py-4 
+     text-white fixed-bottom"
+    >
+      <div className="container text-center">
+        <small>Copyright &copy; Paris Arcos &amp; Paul Molano</small>
+      </div>
+    </footer>
+  );
+};
